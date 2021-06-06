6fda5939cfa02ffd4c844882f3067e32c82c4809
zandy <zandy8013@foxmail.com>
2021-06-07 05:04:50 +0800

    update.

--- src/main/java/cn/zandy/je/jdk/cache/StringPoolTest.java
+++ src/main/java/cn/zandy/je/jdk/cache/StringPoolTest.java
@@ -1,25 +1,34 @@
 package cn.zandy.je.jdk.cache;
 
 /**
- * https://tech.meituan.com/2014/03/06/in-depth-understanding-string-intern.html
+ * <pre>
+ *   symbolTable.hpp
+ *       // CONSTANT_Utf8 -> Symbol*（一个指针，指向一个 Symbol 类型的 C++ 对象，内容是跟 Class 文件同样格式的 UTF-8 编码的字符串）
+ *       class SymbolTable : public RehashableHashtable<Symbol*, mtSymbol> {...};
  *
- * ==========================
- * https://www.zhihu.com/question/64726158
- * https://www.zhihu.com/question/60778124/answer/180076902
+ *       // CONSTANT_String -> java.lang.String（一个实际的 Java 对象的引用，C++ 类型是 oop）
+ *       class StringTable : public RehashableHashtable<oop,     mtSymbol> {...};
+ * </pre>
+ *
  * 【ldc 指令的说明】
+ * 参考：https://www.zhihu.com/question/55994121/answer/408891707
+ *
  * 对于一个字符串比如 String s = "abc";:
- * ① 在类加载的【加载】阶段，"abc" 最终作为一个 UTF8 的字面量被塞进了符号表 SymbolTable 中，并映射到运行时常量池
+ *
+ * ① 在类加载的过程中，"abc" 最终作为一个 UTF8 的字面量被塞进了符号表 SymbolTable 中，并映射到运行时常量池
  * ② 在指令执行阶段，ldc 指令在将常量 push 进操作数栈之前，会做以下事情：
- * ·  - 1.根据 ldc 的参数 (CONSTANT_String_info 类型) 去运行时常量池 SymbolTable 中找到对应的 tag 的 Symbol 对象
- * ·  - 2.通过 StringTable::intern 将 Symbol 转换成 java_lang_String 类型的字符串对象
- * ·  - 3.然后查看 StringTable 中有没有该字符串
- * ·      - 3.1.如果有，将字符串对象直接返回
- * ·      - 3.2.如果没有，将字符串对象先加入到 StringTable 中，然后返回该对象
- * ③ 将返回的字符串对象压栈
+ * 到当前类的运行时常量池（runtime constant pool, HotSpot VM 里是 ConstantPool + ConstantPoolCache）去查找该 index 对应的项，
+ * 如果该项尚未 resolve 则 resolve 之，并返回 resolve 后的内容。
+ *
+ * 在遇到 String 类型常量时，resolve 的过程如果发现 StringTable 已经有了内容匹配的 java.lang.String 的引用，则直接返回这个引用；
+ * 反之，如果 StringTable 里尚未有内容匹配的 String 实例的引用，则会在 Java 堆里创建一个对应内容的 String 对象，
+ * 然后在 StringTable 记录下这个引用，并返回这个引用出去。
+ *
+ * ③ 将返回的引用压栈
  *
  * 【回答之前的疑问】<pre> new String("2") + new String("2"); </pre>
- * Q: 执行完这段代码，为什么在字符串常量池中没有 "22"?
- * A: 因为 stringBuilder.toString 时，不会使用 ldc 指令，所以只是在堆中创建一个对象
+ * Q: 执行完这段代码，为什么在 StringTable 中没有指向 interned string "22" 的引用?
+ * A: 因为代码中没有字面量"22", stringBuilder.toString 时，不会使用 ldc 指令，所以只是在堆中创建一个对象
  *
  * ==========================
  * https://www.iteye.com/blog/rednaxelafx-774673 (10年的文章，下面的评论比较"激烈"。我对其中 "ldc指令……"的描述有异议)
@@ -37,35 +46,35 @@ public class StringPoolTest {
     }
 
     private static void test1() {
-        String s = new String("abc");           // 生成了 2 个对象：字符串常量池中的 "abc" 和 堆中对象. s 指向后者
+        String s = new String("abc");           // 生成了 2 个对象：interned string 和 new String, 都在堆中. s 指向后者
         // 0 new            - 这里是在堆中创建对象
         // 3 dup
-        // 4 ldc #4 <abc>   - 这里会先向 字符串常量池中加入 "abc" 对象
+        // 4 ldc #4 <abc>   - 这里是在堆中创建了 interned string 对象
         // invokespecial
 
-        String s1 = "abc";                      // 相当于 new String("abc").intern();             s1 指向前者
-        String s2 = new String("abc").intern(); //                                               s2 指向前者
+        String s1 = "abc";                      //               s1 指向前者
+        String s2 = new String("abc").intern(); //               s2 指向前者
 
         System.out.println(s == s1);  // false
         System.out.println(s == s2);  // false
         System.out.println(s1 == s2); // true
     }
 
-    // 运行时常量池一直在方法区 (Method Area), 里面包含了每一个.class文件中的常量池中的内容
-    // 而【字符串常量池】和【静态变量】在 Java 7 之前保存在方法区，在 Java 7 之后保存在堆上
+    // 【interned string】和【静态变量】在 Java 7 之前保存在方法区，在 Java 7 之后保存在堆上
+    // jdk8
     private static void test2() {
         String str1 = new String("1");
         str1.intern(); // 上面 new String("1") 中， ldc 指令已经调用了 intern; 所以这行是没啥用的了。
         String str2 = "1";
         System.out.println(str1 == str2); // false
 
-        // 注 *** 以下注释是针对 Java 7 之后的说明 ***
-        String str3 = new String("2") + new String("2"); // 最终是 2 个对象：字符串常量池中的 "2" 和 堆中的字符串对象(value=22).  str3 指向后者
-        str3.intern();        // 字符串常量池中没有 "22", 将堆中的字符串对象(value=22)的引用也就是 str3 放到了字符串常量池中
-        String str4 = "22";   // str4 指向字符串常量池中的引用，即最终是指向堆中的字符串对象(value=22)
+        String str3 = new String("2") + new String("2");
+        str3.intern();
+        String str4 = "22";
         System.out.println(str3 == str4); // true
     }
 
+    // jdk8
     private static void test3() {
         String str1 = new String("1");
         String str2 = "1";
@@ -73,8 +82,8 @@ public class StringPoolTest {
         System.out.println(str1 == str2); //false
 
         String str3 = new String("2") + new String("2");
-        String str4 = "22"; // 字符串常量池中没有 "22", 这时在字符串常量池中创建一个 "22" 对象
-        str3.intern();      // 已经有了，什么也不干
+        String str4 = "22";
+        str3.intern();
         System.out.println(str3 == str4); // false
     }
 
